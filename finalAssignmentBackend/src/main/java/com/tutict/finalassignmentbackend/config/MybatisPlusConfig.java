5acfd999bd5fb02525e55808749b49a17f651aec
tutict <tutict@163.com>
2024-03-15 16:32:59 +0800

    20240314-1

---
+++ finalAssignmentBackend/src/main/java/com/tutict/finalassignmentbackend/config/MybatisPlusConfig.java
@@ -0,0 +1,22 @@
+package com.tutict.finalassignmentbackend.config;
+
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Description: MybatisPlusConfig
+ * @Version: 1.0
+ */
+@Configuration
+public class MybatisPlusConfig {
+
+    /**
+     * 分页插件
+     * @return
+     */
+    @Bean
+    public PaginationInnerInterceptor paginationInnerInterceptor() {
+        return new PaginationInnerInterceptor();
+    }
+}
---
+++ finalAssignmentBackend/src/main/java/com/tutict/finalassignmentbackend/controller/LocationController.java
@@ -0,0 +1,29 @@
+package com.tutict.finalassignmentbackend.controller;
+
+import com.tutict.finalassignmentbackend.utils.GaoDeGeocoder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class LocationController {
+
+//    @Autowired
+    @Autowired
+    private GaoDeGeocoder gaoDeGeocoder;
+
+//    @PostMapping("/location")
+    @PostMapping("/location")
+    public String handleLocation(@RequestBody Map<String, Object> locationData) throws Exception {
+
+        double longitude = (double) locationData.get("laitude");
+        double latitude = (double) locationData.get("longitude");
+
+        String address = gaoDeGeocoder.getAddress(longitude, latitude);
+
+        return "你的地址是" + address;
+    }
+}
