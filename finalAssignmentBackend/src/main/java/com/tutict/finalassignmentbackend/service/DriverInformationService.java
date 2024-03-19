1f3c44f7f4a489cc3f2a71876f01163938ba3d51
Tutict White <70006802+tutict@users.noreply.github.com>
2024-03-19 22:44:59 +0800

    20240319-4

--- finalAssignmentBackend/src/main/java/com/tutict/finalassignmentbackend/service/DriverInformationService.java
+++ finalAssignmentBackend/src/main/java/com/tutict/finalassignmentbackend/service/DriverInformationService.java
@@ -1,8 +1,12 @@
 package com.tutict.finalassignmentbackend.service;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.tutict.finalassignmentbackend.dao.DriverInformationMapper;
+import com.tutict.finalassignmentbackend.entity.DriverInformation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class DriverInformationService {
@@ -13,4 +17,46 @@ public class DriverInformationService {
     public DriverInformationService(DriverInformationMapper driverInformationMapper) {
         this.driverInformationMapper = driverInformationMapper;
     }
+
+    public void createDriver(DriverInformation driverInformation) {
+        driverInformationMapper.insert(driverInformation);
+    }
+
+    public DriverInformation getDriverById(int driverId) {
+        return driverInformationMapper.selectById(driverId);
+    }
+
+    public List<DriverInformation> getAllDrivers() {
+        return driverInformationMapper.selectList(null);
+    }
+
+    public void updateDriver(DriverInformation driverInformation) {
+        driverInformationMapper.updateById(driverInformation);
+    }
+
+    public void deleteDriver(int driverId) {
+        driverInformationMapper.deleteById(driverId);
+    }
+
+    // get driver by id card number
+    public List<DriverInformation> getDriversByIdCardNumber(String idCardNumber) {
+        QueryWrapper<DriverInformation> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("id_card_number", idCardNumber);
+        return driverInformationMapper.selectList(queryWrapper);
+    }
+
+    // get driver by driver license number
+    public DriverInformation getDriverByDriverLicenseNumber(String driverLicenseNumber) {
+        QueryWrapper<DriverInformation> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("driver_license_number", driverLicenseNumber);
+        return driverInformationMapper.selectOne(queryWrapper);
+    }
+
+    // get driver by name
+    public List<DriverInformation> getDriversByName(String Name) {
+        QueryWrapper<DriverInformation> queryWrapper = new QueryWrapper<>();
+        queryWrapper.like("name", Name);
+        return driverInformationMapper.selectList(queryWrapper);
+    }
 }
+
--- finalAssignmentBackend/src/main/java/com/tutict/finalassignmentbackend/service/FineInformationService.java
+++ finalAssignmentBackend/src/main/java/com/tutict/finalassignmentbackend/service/FineInformationService.java
@@ -1,8 +1,13 @@
 package com.tutict.finalassignmentbackend.service;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.tutict.finalassignmentbackend.dao.FineInformationMapper;
+import com.tutict.finalassignmentbackend.entity.FineInformation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
 
 @Service
 public class FineInformationService {
@@ -12,5 +17,45 @@ public class FineInformationService {
     @Autowired
     public FineInformationService(FineInformationMapper fineInformationMapper) {
         this.fineInformationMapper = fineInformationMapper;
+    }
+
+    public void createFine(FineInformation fineInformation) {
+        fineInformationMapper.insert(fineInformation);
+    }
+
+    public FineInformation getFineById(int fineId) {
+        return fineInformationMapper.selectById(fineId);
+    }
+
+    public List<FineInformation> getAllFines() {
+        return fineInformationMapper.selectList(null);
+    }
+
+    public void updateFine(FineInformation fineInformation) {
+        fineInformationMapper.updateById(fineInformation);
+    }
+
+    public void deleteFine(int fineId) {
+        fineInformationMapper.deleteById(fineId);
+    }
+
+    // get all fines by payee
+    public List<FineInformation> getFinesByPayee(String payee) {
+        QueryWrapper<FineInformation> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("payee", payee);
+        return fineInformationMapper.selectList(queryWrapper);
+    }
+
+    // get all fines by time range
+    public List<FineInformation> getFinesByTimeRange(Date startTime, Date endTime) {
+        QueryWrapper<FineInformation> queryWrapper = new QueryWrapper<>();
+        queryWrapper.between("fineTime", startTime, endTime);
+        return fineInformationMapper.selectList(queryWrapper);
+    }
+
+    public FineInformation getFineByReceiptNumber(String receiptNumber) {
+        QueryWrapper<FineInformation> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("receiptNumber", receiptNumber);
+        return fineInformationMapper.selectOne(queryWrapper);
     }
 }
