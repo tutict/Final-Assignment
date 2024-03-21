3748f59e37bf9b23dccadb2e650715c8a333ec87
tutict <tutict@163.com>
2024-03-21 17:00:15 +0800

    20240321-3

--- finalAssignmentBackend/src/main/java/com/tutict/finalassignmentbackend/FinalAssignmentBackendApplication.java
+++ finalAssignmentBackend/src/main/java/com/tutict/finalassignmentbackend/FinalAssignmentBackendApplication.java
@@ -2,6 +2,9 @@ package com.tutict.finalassignmentbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class FinalAssignmentBackendApplication {
@@ -10,4 +13,19 @@ public class FinalAssignmentBackendApplication {
         SpringApplication.run(FinalAssignmentBackendApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .exposedHeaders("Authorization")
+                        .allowCredentials(true)
+                        .maxAge(3600);
+            }
+        };
+    }
 }
