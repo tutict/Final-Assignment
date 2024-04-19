e774a577feeaa446f7de7b572f087cc6a30d01d8
Tutict White <70006802+tutict@users.noreply.github.com>
2024-04-19 14:14:17 +0800

    加入kafka和vert.x配置-2

---
+++ finalAssignmentBackend/src/main/java/com/tutict/finalassignmentbackend/config/vertx/VertxConfig.java
@@ -0,0 +1,15 @@
+package com.tutict.finalassignmentbackend.config.vertx;
+
+import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class VertxConfig {
+
+    @Bean
+    public Vertx vertx() {
+        return Vertx.vertx(new VertxOptions());
+    }
+}
