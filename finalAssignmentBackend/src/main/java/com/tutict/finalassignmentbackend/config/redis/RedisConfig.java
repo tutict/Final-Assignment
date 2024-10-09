5e7e93e8ce8c0ea4dd1b396a6821dc0250c690c1
Tutict White <70006802+tutict@users.noreply.github.com>
2024-10-09 16:19:05 +0800

    10.9-2

--- finalAssignmentBackend/src/main/java/com/tutict/finalassignmentbackend/config/redis/RedisConfig.java
+++ finalAssignmentBackend/src/main/java/com/tutict/finalassignmentbackend/config/redis/RedisConfig.java
@@ -5,13 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import java.time.Duration;
 
