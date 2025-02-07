c0086bd772e422d6a835d8c939ead7aa50858984
tutict <tutict@163.com>
2025-02-07 17:54:49 +0800

    2-7:4

--- final_assignment_backend_quarkus/src/test/java/bagu/design/AbstractFactory.java
+++ final_assignment_backend_quarkus/src/test/java/bagu/design/AbstractFactory.java
@@ -1,6 +1,11 @@
 package bagu.design;
 
 
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Warmup;
+
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -90,6 +95,10 @@ public class AbstractFactory {
         desk.check();
     }
 
+    @Benchmark
+    @Fork(value = 1)                // 启动一个独立的 JVM 进程运行测试
+    @Warmup(iterations = 2)         // 预热5次（预热阶段不计入最终数据）
+    @Measurement(iterations = 1)
     public static void main(String[] args) {
         // 获取当前操作系统的名称
         String os = System.getProperty("os.name").toLowerCase();
--- final_assignment_backend_quarkus/src/test/java/bagu/design/Decorator.java
+++ final_assignment_backend_quarkus/src/test/java/bagu/design/Decorator.java
@@ -1,5 +1,10 @@
 package bagu.design;
 
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Warmup;
+
 import java.util.logging.Logger;
 
 /*
@@ -8,6 +13,10 @@ import java.util.logging.Logger;
 public class Decorator {
     private static final Logger logger = Logger.getGlobal();
 
+    @Benchmark
+    @Fork(value = 1)                // 启动一个独立的 JVM 进程运行测试
+    @Warmup(iterations = 2)         // 预热5次（预热阶段不计入最终数据）
+    @Measurement(iterations = 1)
     public static void main(String[] args) {
 
     }
